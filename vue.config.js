6c2631e8e47b2bb6d03a2c08d544b388cc160ab9
admin <3091965883@qq.com>
2021-06-16 15:33:34 +0800

    新增cookie封装，request封装

--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
         },
         proxy: {
             '/api': {
-                target: '<url>',
+                target: '',
                 pathRewrite: {
 
                 },
